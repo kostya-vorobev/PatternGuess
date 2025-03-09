12242e0dd4d17f4a8933d02a84ac431e45016a55
kostya-vorobev <105592308+kostya-vorobev@users.noreply.github.com>
2025-03-09 21:56:46 +0700

    Тест на наличие класса

---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using PatternGuess;
+
+namespace UnitTestProject
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void PatternClass()
+        {
+            Pattern pattern = new Pattern();
+            Assert.NotNull(pattern);
+        }
+    }
+}
