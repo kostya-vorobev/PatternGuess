a6d6beb7b09050009a56afeb3f925f5e1d28902d
kostya-vorobev <105592308+kostya-vorobev@users.noreply.github.com>
2025-03-09 22:33:36 +0700

    Возврат верного ответа успешен

--- PatternGuess/Pattern.cs
+++ PatternGuess/Pattern.cs
@@ -8,9 +8,23 @@ namespace PatternGuess
 {
     public class Pattern
     {
+        string veryAnswer;
+        private readonly Random random = new Random();
+
+        private readonly List<string> patternNames = new List<string>
+        {
+            "Singleton",
+            "Factory",
+            "Observer",
+            "Decorator",
+            "Strategy"
+        };
+
         public Pattern()
         {
-
+            this.VeryAnswer = patternNames[random.Next(patternNames.Count())];
         }
+
+        public string VeryAnswer { get => veryAnswer; set => veryAnswer = value; }
     }
 }
